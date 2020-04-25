9d0653fd4fd148b2f025f1aedf318f729367171f
Susan Qu <susan@susanqu.com>
2020-04-25 14:34:35 -0400

    updated background color for curve variation1

--- parametric_equation_sketch_3/curve_variation_color1.py
+++ parametric_equation_sketch_3/curve_variation_color1.py
@@ -1,35 +1,51 @@
 #########################################
-# Curve
+# Curve - colour
 #########################################
 
 t = 0
 DELAY = 5 * 100
+r = 222
+g = 0
+b = 0
 
 def setup():
     background(242, 242, 242)
-    size(800, 800)
+    size(800, 800, P2D)
+
 
 def draw():
 
-    background(242, 242, 242)
+    #background(242, 242, 242)
+
+    setGradient()
     global t
 
     translate(width / 2, height / 2)
 
 
     for i in range(30):
-
-        stroke(204, 102, 0, i*5)
-        fill(204, 102, 0, i*5)
+        #stroke(204, 102, 0, i*5)
+        #fill(204, 102, 0, i*5)
+        fill(121, 69, 37, i*5)
+        stroke(71, 46, 21, i*5)
         strokeWeight(2)
 
         curve(x(t + i), y(t + i), x1(t + i), y1(t + i),
              x(t - i), y(t - i), x1(t - i), y1(t - i))
 
-
-
-
     t = t + 0.1
+
+def setGradient():
+    beginShape()
+    fill(255)
+    vertex(0, 0)
+    vertex(width, 0)
+    strokeWeight(0)
+    fill(204, 102, 0, 0.25)
+    vertex(width, height)
+    vertex(0, height)
+    strokeWeight(0)
+    endShape(CLOSE)
 
 
 def mousePressed():
