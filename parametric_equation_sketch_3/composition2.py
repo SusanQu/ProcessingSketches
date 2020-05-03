21bd98c5f1f3f7c71bf3e375e27f83dc5779c3e4
Susan Qu <susan@susanqu.com>
2020-05-02 21:53:48 -0400

    updated composition2 and added other line variations

--- parametric_equation_sketch_3/composition2.py
+++ parametric_equation_sketch_3/composition2.py
@@ -5,6 +5,7 @@
 t = 0
 p = 0
 DELAY = 5 * 100
+xoff = float(0.0)
 
 def setup():
     background(242, 242, 242)
@@ -15,22 +16,53 @@ def draw():
     background(242, 242, 242)
     global t
     global p
+    global xoff
 
     x = 0
     j = 0
+    e = 0
+    xoff = xoff + 1
+
+
     while(x < width):
-        line(x, 0, x, 400)
-        x = x + 5
+        line(x , 0, x , 400)
+        stroke(51, 51, 51)
+        strokeWeight(2)
+        x = x + 3
+    '''
+    while(e < width):
+        line(100, 200, e/3, 400)
+        stroke(51, 51, 51)
+        strokeWeight(e/500)
+        e = e + 5
+    '''
 
     while(j < (width + 300)):
-        line(j, 400, 200, j)
+        line(j, 400, 0, j)
+        strokeWeight(1)
         j = j + 10
 
-    obj()
 
+    '''
+    fill(242, 242, 242)
+    noStroke()
+    arc(590, 400, 150, 180, PI, TWO_PI)
 
+    fill(242, 242, 242, 191)
+    noStroke()
+    arc(590, 400, 220, 250, PI, TWO_PI)
+
+    fill(242, 242, 242, 127)
+    noStroke()
+    arc(590, 400, 330, 350, PI, TWO_PI)
+    '''
+    obj(550, 430)
+    obj1(130, -150)
+    cloud()
 
     t = t + 0.1
+
+
 
 
 def mousePressed():
@@ -41,23 +73,74 @@ def pauseFrame():
 
 
 
-def obj():
-    translate(550, 420)
+def obj(transX, transY):
+    translate(transX, transY)
 
-    for i in range(20):
-        stroke(51, 51, 51, i*10)
-        fill(51, 51, 51)
+    for i in range(40):
+        stroke(242, 242, 242, i*10)
+        fill(242, 242, 242)
         strokeWeight(2)
-        ellipse(xx(t + i), yy(t + i), xx1(t + i), yy1(t + i))
+        ellipse(xx(t + i), yy(t + i), xx1(t + i, 400), yy1(t + i))
+
+def obj1(transX, transY):
+    translate(transX, transY)
+
+    for i in range(50):
+        stroke(242, 242, 242, i*2)
+        fill(242, 242, 242)
+        strokeWeight(1)
+        ellipse(xxx(t + i), yyy(t + i), xxx1(t + i), yyy1(t + i))
+
+def cloud():
+    translate(-250, -100)
+    for s in range(40):
+        stroke(242, 242, 242, s*3)
+        noFill()
+        #fill(242, 242, 242, s*10)
+        strokeWeight(4)
+        bezier(xs(t + s), ys(t + s), xs1(t +s), ys1(t+s),
+                 xs(t -s), ys(t - s), xs1(t - s), ys1(t-s))
+
+
 
 def xx(t):
-    return cos(t / 10) + sin(-t/10) * 100
+    return cos(t / 10) + sin(-t/10) * 125
 
 def yy(t):
     return sin(t / 10) + sin(t/10) * 50
 
-def xx1(t):
-    return cos(t /10) * 200
+def xx1(t, w):
+    return cos(t /10) * w
 
 def yy1(t):
     return cos(t / 10)
+
+
+
+def xxx(t):
+    return sin(t / 10)
+
+def yyy(t):
+    return cos(t / 10) + sin(-t/10) * 50
+
+def xxx1(t):
+    return cos(t / 10)  * 100
+
+def yyy1(t):
+    return cos(t /10)
+
+
+def xs(t):
+    return cos(t / 10) * 200
+
+def ys(t):
+    return sin(-t / 10) * 200  - sin(-t / 20) * 100
+
+def xs1(t):
+    return cos(-t /10) * 300
+
+def ys1(t):
+    return sin(t / 10) * 120 + cos(-t /10) * 200
+
+def xs2(t):
+    return cos(t / 10) * 100 + cos(-t /10) * 200
---
+++ parametric_equation_sketch_3/line_flower_1.py
@@ -0,0 +1,54 @@
+#########################################
+# Line - Flower
+#########################################
+
+t = 0
+DELAY = 5 * 100
+
+def setup():
+    background(242, 242, 242)
+    size(800, 800)
+
+def draw():
+
+    background(242, 242, 242)
+    global t
+
+    for j in range(1, 10):
+        leaf(2, 2, 1, 0)
+        leaf(2, 2, 1, 180)
+        leaf(2, 2, 1, 180)
+
+    t = t + 0.13
+
+
+def leaf(transX, transY, scale_val, rotate_val):
+    translate(width / transX, height / transY)
+    scale(scale_val)
+    rotate(rotate_val)
+
+    for i in range(20):
+        stroke(51, 51, 51, i * 10)
+        fill(51, 51, 51, i * 3)
+        strokeWeight(2)
+        line(x(t + i), y(t + i), x1(t + i), y1(t + i))
+
+
+def mousePressed():
+    pauseFrame()
+    saveFrame('movie_frames/lineDots_SingleFrame/lineDots_####.png')
+
+def pauseFrame():
+    delay(DELAY)
+
+def x(t):
+    return sin(t/10) * 100
+
+def y(t):
+    return cos(t)
+
+def x1(t):
+    return sin(t/10) * 200
+
+def y1(t):
+    return cos(t/10) * 200
